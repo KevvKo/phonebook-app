ebace09172c6bd7dfd98b38c3f664c0bd56404e3
KevvKo <kklein915@gmail.com>
2021-10-12 22:00:23 +0200

    minor changes for filterArray function

--- api/src/assets/utils.js
+++ api/src/assets/utils.js
@@ -1,5 +1,11 @@
-const filterArray = ( arra, query ) => {
-    return arra.filter( ( entry ) => {
+/**
+ * filters an array with a given query to find all Object with the belonging key (includes prefixes)
+ * @param { Array } array  
+ * @param { String } query 
+ * @returns Array
+ */
+const filterArray = ( array, query ) => {
+    return array.filter( ( entry ) => {
         console.log(entry.name.toLowerCase().indexOf( query.toLowerCase() ) !== -1);
         return entry.name.toLowerCase().indexOf( query.toLowerCase() ) !== -1;
     });
