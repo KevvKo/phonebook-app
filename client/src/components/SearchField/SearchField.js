71bb2cc4e243a073e73aa7f7d56417a6c3dc7a8a
Kevin Klein <k.klein@intershop.de>
2021-10-13 21:30:39 +0200

    add query to searchfield

--- client/src/components/SearchField/SearchField.js
+++ client/src/components/SearchField/SearchField.js
@@ -3,12 +3,22 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 // Hooks
 import { useTranslation } from 'react-i18next';
+import { useQuery } from '@apollo/client';
+// GQL
+import { PHONE_BOOK_QUERY } from '../../graphql/phonebook/phoneBookQuery';
 
 export default function SearchField() {
 
   const [ t ] = useTranslation('common');
   
+  function queryPoneBook( e ) {
+    const query = e.target.value;
+    const {error, loading, data } = useQuery( PHONE_BOOK_QUERY, {
+      variables: { query }
+    });
+  }
+
   return (
-      <TextField id="outlined-basic" label={ t('searchField.label') } variant="filled" />
+      <TextField id="outlined-basic" label={ t('searchField.label') } variant="filled" onChange={ queryPoneBook } />
   );
 }
--- client/src/graphql/phonebook/phoneBookQuery.js
+++ client/src/graphql/phonebook/phoneBookQuery.js
@@ -2,9 +2,11 @@ import { gql } from '@apollo/client';
 
 export const PHONE_BOOK_QUERY = gql`
     {
-        phoneBook {
-            name
-            number
+        query phoneBookEntryss($query: String!) {
+            phoneBookEntry(query: query){
+                name
+                number
+            }
         }
     }
 `;
