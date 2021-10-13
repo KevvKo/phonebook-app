ecddf71ba567f07605ee10e5c73c21d80c755277
KevvKo <kklein915@gmail.com>
2021-10-13 17:32:16 +0200

    minor changes for dependencies and add searchfield component

---
+++ client/src/components/SearchField/SearchField.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SearchField from './SearchField';
+
+test('renders learn react link', () => {
+  render(<SearchField />);
+});
