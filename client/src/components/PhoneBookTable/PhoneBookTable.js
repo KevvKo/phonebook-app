6d0d80b34cd96594de5908de64de2271b5ad6da9
KevvKo <kklein915@gmail.com>
2021-10-14 16:51:17 +0200

    add missing changes

--- client/src/components/PhoneBookTable/PhoneBookTable.js
+++ client/src/components/PhoneBookTable/PhoneBookTable.js
@@ -1,19 +1,46 @@
 import React from 'react';
+// Components
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 // Hooks
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 
-export default function PhoneBookTable(props) {
+export default function PhoneBookTable() {
 
   const [ t ] = useTranslation('common');
   const phoneBook = useSelector( state => state.phoneBook );
   const phoneBookEntrys = phoneBook.phoneBook;
 
   return (
-    <>
-        { phoneBookEntrys &&
-          phoneBookEntrys.map( (c, i) => <div key={i}>{c.name}</div>)
-        }  
-    </>
+    
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell> Name </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          { phoneBookEntrys &&
+            phoneBookEntrys.map ( (c, i) =>  (
+              <TableRow
+                key={ c.name }
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  { c.name }
+                </TableCell>
+              </TableRow>
+            ))
+          }
+        </TableBody>
+      </Table>
+    </TableContainer> 
   );
 }
